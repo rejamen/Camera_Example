4765eab30fed1cf9870c572b40f2313eff9f1275
Reinaldo J <rejamen@gmail.com>
2019-05-23 22:26:56 -0400

    Take picture and show in ImageView. Dont save in storage

---
+++ app/src/main/java/com/aidooit/camera_example/CameraActivity.java
@@ -0,0 +1,46 @@
+package com.aidooit.camera_example;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CameraActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+
+        Button button = findViewById(R.id.btnTakePicture);
+        imageView = findViewById(R.id.imageViewId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+
+
+    }
+}
