87e0fe904178cff778ade0e58c76378cfd4d4882
Reinaldo Javier Menendez <rejamen@gmail.com>
2019-05-26 17:33:50 -0400

    Photo taken and saved auto to Gallery

--- app/src/main/java/com/aidooit/camera_example/CameraActivity.java
+++ app/src/main/java/com/aidooit/camera_example/CameraActivity.java
@@ -1,14 +1,23 @@
 package com.aidooit.camera_example;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -26,7 +35,7 @@ public class CameraActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
                 startActivityForResult(intent, 0);
             }
 
@@ -41,6 +50,42 @@ public class CameraActivity extends AppCompatActivity {
         Bitmap bitmap = (Bitmap)data.getExtras().get("data");
         imageView.setImageBitmap(bitmap);
 
+        /*imageView.buildDrawingCache();
+
+        //Bitmap bmp = imageView.getDrawingCache();
+
+        //to get the image from the ImageView (say iv)
+        BitmapDrawable draw = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = draw.getBitmap();
+
+        FileOutputStream outStream = null;
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdCard.getAbsolutePath() + "/YourFolderName");
+        dir.mkdirs();
+        String fileName = String.format("%d.jpg", 10);
+        File outFile = new File(dir, fileName);
+
+
+        try{
+            outStream = new FileOutputStream(outFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
+
+
+
+
+
 
     }
+
+
 }
